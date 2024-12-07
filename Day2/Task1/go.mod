13c78d0ca95d670a6ede635b3ca93898a0eaf512
PiotrJurek <piotrjurek9@gmail.com>
2024-12-07 18:58:14 +0100

    Day 2 Go

---
+++ Day2/Task1/go.mod
@@ -0,0 +1,3 @@
+module day2/task1
+
+go 1.23.4
---
+++ Day2/Task1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func textToNumbers(text string) []int {
+	numbers := strings.Split(text, " ")
+	output := []int{}
+
+	for i := 0; i < len(numbers); i++ {
+		number, _ := strconv.Atoi(numbers[i])
+		output = append(output, number)
+	}
+
+	return output
+}
+
+func isSafe(numbers []int) bool {
+	increasing := false
+	decreasing := false
+
+	for i := 0; i < len(numbers)-1; i++ {
+		difference := numbers[i+1] - numbers[i]
+
+		if difference > 3 || difference < -3 || difference == 0 {
+			return false
+		}
+
+		if difference > 0 {
+			if decreasing {
+				return false
+			}
+			increasing = true
+		}
+
+		if difference < 0 {
+			if increasing {
+				return false
+			}
+			decreasing = true
+		}
+	}
+
+	return true
+}
+
+func main() {
+	file, err := os.Open("../Inputs/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	counter := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		numbers := textToNumbers(text)
+		if isSafe(numbers) {
+			counter++
+		}
+	}
+
+	print(counter)
+}
